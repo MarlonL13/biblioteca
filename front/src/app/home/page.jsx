f598f43a49835a5e9c8c949e5c3eaf8c0aa37dfc
MATHEUS PIMENTEL VIEIRA <a92263706@rn.senac.local>
2025-06-23 20:23:48 -0300

    oi

--- front/src/app/home/page.jsx
+++ front/src/app/home/page.jsx
@@ -2,10 +2,20 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import BookCard from "../components/BookCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route"
 
-export default function Home() {
+export default async function Home() {
+  
+  const session = await getServerSession(authOptions)
+  
+  if (!session) {
+    return(
+      <div className="text-red-700" >VOCÊ NÃO ESTÁ LOGADO</div>
+    )
+  }
+
   const [books, setBooks] = useState([]);
-
   useEffect(() => {
     axios.get("http://localhost:3000/tasks")
       .then(res => setBooks(res.data))
--- front/src/app/page.jsx
+++ front/src/app/page.jsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
+'use client'
+import {signIn} from "next-auth/react"
 
-export default function Home() {
+export default function Login() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Trabalhando com autenticacao</h1>
         <div className="mt-3.5" >
-        <button className="bg-blue-700 rounded-2xl text-white p-3 hover:bg-amber-600 cursor-pointer" onClick={() => signIn("github",{callbackUrl: "/dashboard"})}>
+        <button className="bg-blue-700 rounded-2xl text-white p-3 hover:bg-amber-600 cursor-pointer" onClick={() => signIn("github",{callbackUrl: "/home"})}>
             Entrar com GitHub
           </button>
         </div>
