6e03e588d141139345e2ba4c1ae82124bbdf2415
MATHEUS PIMENTEL VIEIRA <a92263706@rn.senac.local>
2025-06-24 19:54:38 -0300

    teste

--- front/src/app/auth/page.js
+++
@@ -1,7 +0,0 @@
-export default function AuthPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold">Auth Page (Coming Soon)</h1>
-    </div>
-  );
-}
--- front/src/app/page.jsx
+++ front/src/app/page.jsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Trabalhando com autenticacao</h1>
+        <h1>📚 Biblioteca</h1>
         <div className="mt-3.5" >
         <button className="bg-blue-700 rounded-2xl text-white p-3 hover:bg-amber-600 cursor-pointer" onClick={() => signIn("github",{callbackUrl: "/home"})}>
             Entrar com GitHub
